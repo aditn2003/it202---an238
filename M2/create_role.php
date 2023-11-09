f0994aab48c6e84cbd3fdcb9004e26772278f027
aditn2003 <an238@njit.edu>
2023-11-08 19:30:46 -0500

    m1flash

--- M2/create_role.php
+++ M2/create_role.php
@@ -2,27 +2,31 @@
 //note we need to go up 1 more directory
 require(__DIR__ . "/partials/nav.php");
 
+require_once(__DIR__ . "/partials/flash.php");
+
 if (!has_role("Admin")) {
-    //flash("You don't have permission to view this page", "warning");
+    flash("You don't have permission to view this page", "warning");
     //die(header("Location: " . get_url("home.php")));
 }
+
+flash("You don't have permission to view this page", "warning");
 
 if (isset($_POST["name"]) && isset($_POST["description"])) {
     $name = se($_POST, "name", "", false);
     $desc = se($_POST, "description", "", false);
     if (empty($name)) {
-        //flash("Name is required", "warning");
+        flash("Name is required", "warning");
     } else {
         $db = getDB();
         $stmt = $db->prepare("INSERT INTO Roles (name, description, is_active) VALUES(:name, :desc, 1)");
         try {
             $stmt->execute([":name" => $name, ":desc" => $desc]);
-            //flash("Successfully created role $name!", "success");
+            flash("Successfully created role $name!", "success");
         } catch (PDOException $e) {
             if ($e->errorInfo[1] === 1062) {
-                //flash("A role with this name already exists, please try another", "warning");
+                flash("A role with this name already exists, please try another", "warning");
             } else {
-                //flash(var_export($e->errorInfo, true), "danger");
+                flash(var_export($e->errorInfo, true), "danger");
             }
         }
     }
@@ -42,5 +46,4 @@ if (isset($_POST["name"]) && isset($_POST["description"])) {
 </form>
 <?php
 //note we need to go up 1 more directory
-//require_once(__DIR__ . "/partials/flash.php");
 ?>
